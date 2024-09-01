f62b7e808bbf7ef32a8a3a7485f3d0d6d787291d
Aspitrine <loquente62@gmail.com>
2024-09-01 21:25:59 +0200

    Display error when bad SQL query

--- src/components/InteractionBdd.tsx
+++ src/components/InteractionBdd.tsx
@@ -12,19 +12,32 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { AlertCircle } from "lucide-react";
 
 export default function InteractionBdd() {
   const [query, setQuery] = useState("SELECT 1 AS num;");
   const [result, setResult] = useState<Results>();
+  const [error, setError] = useState<string>();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError(undefined);
 
     const { query } = Object.fromEntries(
       new FormData(e.currentTarget),
     ) as Record<string, string>;
 
-    setResult(await db.query(query));
+    try {
+      setResult(await db.query(query));
+    } catch (error) {
+      setResult(undefined);
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("Une erreur est survenue");
+      }
+    }
   };
 
   const fields = result?.fields.map((field) => field.name);
@@ -35,32 +48,42 @@ export default function InteractionBdd() {
         <TextareaSQL name="query" value={query} onChange={setQuery} />
         <Button type="submit">Run query</Button>
       </form>
-      <Table>
-        <TableCaption>Résultat de la requête</TableCaption>
-        <TableHeader>
-          <TableRow>
-            {fields?.map((field, fieldIndex) => (
-              <TableHead key={`${field}-${fieldIndex}`}>{field}</TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {result?.rows.map((row) => (
+      {error && (
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+
+          <AlertTitle>Erreur</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
+      {result && (
+        <Table>
+          <TableCaption>Résultat de la requête</TableCaption>
+          <TableHeader>
             <TableRow>
-              {fields?.map((field, indexField) => (
-                <TableCell
-                  className="p-2"
-                  key={`${field}-${row.id}-${indexField}`}
-                >
-                  {row[field] instanceof Date
-                    ? row[field].toLocaleDateString()
-                    : row[field]}
-                </TableCell>
+              {fields?.map((field, fieldIndex) => (
+                <TableHead key={`${field}-${fieldIndex}`}>{field}</TableHead>
               ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {result?.rows.map((row) => (
+              <TableRow>
+                {fields?.map((field, indexField) => (
+                  <TableCell
+                    className="p-2"
+                    key={`${field}-${row.id}-${indexField}`}
+                  >
+                    {row[field] instanceof Date
+                      ? row[field].toLocaleDateString()
+                      : row[field]}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </div>
   );
 }
